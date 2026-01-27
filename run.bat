aa6e76579a351b128afb0de8806e00a4076d6b36
Jhon Kharl Victoriano <161316867+MAMAkoblue@users.noreply.github.com>
2026-01-27 19:46:07 +0800

    updates

--- run.bat
+++ run.bat
@@ -6,7 +6,7 @@ if not exist build\classes mkdir build\classes
 
 REM Compile all Java files with classpath
 echo Compiling source files...
-javac -cp "lib\*" -d build\classes src\loginandregister\*.java src\data\*.java src\ui\*.java src\org\mindrot\jbcrypt\*.java src\util\*.java src\service\*.java src\kharlmotorph\*.java
+javac -cp "lib\*;lib\absolutelayout\AbsoluteLayout.jar;lib\JCalendar\jcalendar-1.4.jar" -d build\classes src\loginandregister\*.java src\data\*.java src\ui\*.java src\org\mindrot\jbcrypt\*.java src\util\*.java src\service\*.java src\kharlmotorph\*.java
 
 if %ERRORLEVEL% NEQ 0 (
     echo Compilation failed!
@@ -18,6 +18,6 @@ echo Compilation successful!
 
 REM Run the application
 echo Starting application...
-java -cp "build\classes;lib\*" loginandregister.LoginAndRegister
+java -cp "build\classes;lib\*;lib\absolutelayout\AbsoluteLayout.jar;lib\JCalendar\jcalendar-1.4.jar" loginandregister.LoginAndRegister
 
 pause
